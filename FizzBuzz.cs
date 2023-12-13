f466a2401522045649bf63739ae5a07d3087535e
AdamGitDotNet <adamleon@tlen.pl>
2023-12-13 20:15:04 +0100

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace zadanie_4_13
+{
+    internal class FizzBuzz
+    {
+        private string wynik = string.Empty;
+        public string SprawdzLiczbe(float liczba)
+        {
+            if (liczba % 3 == 0 && liczba % 5 == 0)
+            {
+                wynik = "FizzBuzz, Podaj kolejną liczbę";
+            }
+            else if (liczba % 3 == 0)
+            {
+                wynik = "Fizz, Podaj kolejną liczbę";
+            }
+            else if (liczba % 5 == 0)
+            {
+                wynik = "Buzz, Podaj kolejną liczbę";
+            }
+            else if (liczba % 3 != 0 && liczba % 5 != 0)
+            {
+                wynik = ($"Liczba {liczba} nie jest podzielna przez 3 i 5");
+            }
+            return wynik;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using zadanie_4_13;
+
+namespace zadania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool wyjscieZProgramu = false;
+            try
+            {
+                Console.WriteLine("Podaj liczbę");
+                FizzBuzz fizzBuzz = new FizzBuzz();
+
+                do
+                {
+                    if (float.TryParse(Console.ReadLine(), out float wynik))
+                    {
+                        Console.WriteLine(fizzBuzz.SprawdzLiczbe(wynik));
+                        Console.WriteLine("\nPodaj kolejną liczbę");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Podałeś niepoprawne dane.");
+                        wyjscieZProgramu = true;
+                    }
+                }
+                while (!wyjscieZProgramu);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
